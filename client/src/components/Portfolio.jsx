6dcd5e3f877b892134fb14a81e22799c32ea1dd1
Jamie Jones <jamie.mar.jones14@gmail.com>
2021-06-23 14:07:01 -0400

    css work

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -13,25 +13,34 @@ const Portfolio = () => {
             <h1 id="portfolio">Portfolio</h1>
             <div class="portfolio-card-sec">
             <div class="cards">
-                    <img src={mfsCountdown} alt="MFS Countdown" class="portfolio-img"/>
+                <img src={mfsCountdown} alt="MFS Countdown" class="portfolio-img"/>
                 <h4 class="img-caption">Motherland: Fort Salem Countdown</h4>
                 <h5 class="img-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus accusantium, doloremque numquam ipsum aliquid repudiandae facere odit sequi consequuntur neque, ab animi voluptates. Sequi quisquam vero recusandae omnis eveniet blanditiis?</h5>
-                <button><a href="https://github.com/jamie-jones/MFS-s2-countdown" target="_blank" rel="noreferrer"><h5>GitHub Repository</h5></a></button>
-                <button><a href="http://mfscountdown.com/" target="_blank" rel="noreferrer"><h5>Deployed Application</h5></a></button>
+                <section class="buttons-section">
+                    <a href="https://github.com/jamie-jones/MFS-s2-countdown" target="_blank" rel="noreferrer"><button class="btns"><h6>github repository</h6></button></a>
+                    <div class="divider"/>
+                    <a href="http://mfscountdown.com/" target="_blank" rel="noreferrer"><button class="btns"><h6>deployed application</h6></button></a>
+                </section>
             </div>
             <div class="cards">
                     <img src={mfsQuiz} alt="MFS Quiz" class="portfolio-img"/>
                 <h4 class="img-caption">Motherland: Fort Salem Specialization Quiz</h4>
                 <h5 class="img-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus accusantium, doloremque numquam ipsum aliquid repudiandae facere odit sequi consequuntur neque, ab animi voluptates. Sequi quisquam vero recusandae omnis eveniet blanditiis?</h5>
-                <button><a href="https://github.com/jamie-jones/motherland-fs-quiz" target="_blank" rel="noreferrer"><h5>GutHub Repository</h5></a></button>
-                <button><a href="http://www.mfsquiz.com/" target="_blank" rel="noreferrer"><h5>Deployed Application</h5></a></button>
+                <section class="buttons-section">
+                <a href="https://github.com/jamie-jones/motherland-fs-quiz" target="_blank" rel="noreferrer"><button class="btns"><h6>github repository</h6></button></a>
+                <div class="divider"/>
+                <a href="http://www.mfsquiz.com/" target="_blank" rel="noreferrer"><button class="btns"><h6>deployed application</h6></button></a>
+                </section>
             </div>
             <div class="cards">
                     <img src={positiveThoughts} alt="Positive Thoughts" class="portfolio-img"/>
                 <h4 class="img-caption">Positive Thoughts</h4>
                 <h5 class="img-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus accusantium, doloremque numquam ipsum aliquid repudiandae facere odit sequi consequuntur neque, ab animi voluptates. Sequi quisquam vero recusandae omnis eveniet blanditiis?</h5>
-                <button><a href="https://github.com/jamie-jones/Positive-Thoughts" target="_blank" rel="noreferrer"><h5>GitHub Repository</h5></a></button>
-                <button><a href="http://www.positive-thoughts.org/" target="_blank" rel="noreferrer"><h5>Deployed Application</h5></a></button>
+                <section class="buttons-section">
+                <a href="https://github.com/jamie-jones/Positive-Thoughts" target="_blank" rel="noreferrer"><button class="btns"><h6>github repository</h6></button></a>
+                <div class="divider"/>
+                <a href="http://www.positive-thoughts.org/" target="_blank" rel="noreferrer"><button class="btns"><h6>deployed application</h6></button></a>
+</section>
             </div>
             </div>
             <br/>
@@ -40,40 +49,34 @@ const Portfolio = () => {
                     <img src={coachella} alt="Coachella" class="portfolio-img"/>
                 <h4 class="img-caption">Coachella</h4>
                 <h5 class="img-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus accusantium, doloremque numquam ipsum aliquid repudiandae facere odit sequi consequuntur neque, ab animi voluptates. Sequi quisquam vero recusandae omnis eveniet blanditiis?</h5>
-                <button><a href="https://github.com/jamie-jones/Couchella" target="_blank" rel="noreferrer"><h5>GitHub Repository</h5></a></button>
-                <button><a href="https://jadavianett.github.io/Couchella/" target="_blank" rel="noreferrer"><h5>Deplyoed Application</h5></a></button>
+                <section class="buttons-section">
+                <a href="https://github.com/jamie-jones/Couchella" target="_blank" rel="noreferrer"><button class="btns"><h6>github repository</h6></button></a>
+                <div class="divider"/>
+                <a href="https://jadavianett.github.io/Couchella/" target="_blank" rel="noreferrer"><button class="btns"><h6>deployed application</h6></button></a>
+            </section>
             </div>
             <div class="cards">
                     <img src={justDocIt} alt="Just Doc It" class="portfolio-img"/>
                 <h4 class="img-caption">Just Doc It</h4>
                 <h5 class="img-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus accusantium, doloremque numquam ipsum aliquid repudiandae facere odit sequi consequuntur neque, ab animi voluptates. Sequi quisquam vero recusandae omnis eveniet blanditiis?</h5>
-                <button><a href="https://github.com/jamie-jones/Just-Doc-It" target="_blank" rel="noreferrer"><h5>GitHub Repository</h5></a></button>
-                <button><a href="http://www.just-doc-it.com/" target="_blank" rel="noreferrer"><h5>Deployed Application</h5></a></button>
+                <section class="buttons-section">
+                <a href="https://github.com/jamie-jones/Just-Doc-It" target="_blank" rel="noreferrer"><button class="btns"><h6>github repository</h6></button></a>
+                <div class="divider"/>
+                <a href="http://www.just-doc-it.com/" target="_blank" rel="noreferrer"><button class="btns"><h6>deployed application</h6></button></a>
+            </section>
             </div>
             <div class="cards">
                     <img src={noteTaker} alt="Note Taker" class="portfolio-img"/>
                 <h4 class="img-caption">Note Taker</h4>
                 <h5 class="img-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus accusantium, doloremque numquam ipsum aliquid repudiandae facere odit sequi consequuntur neque, ab animi voluptates. Sequi quisquam vero recusandae omnis eveniet blanditiis?</h5>
-                <button><a href="https://github.com/jamie-jones/Note-Taker-App" target="_blank" rel="noreferrer"><h5>GitHub Repository</h5></a></button>
-                <button><a href="https://note-taker-jamie.herokuapp.com/" target="_blank" rel="noreferrer"><h5>Deployed Application</h5></a></button>
+                <section class="buttons-section">
+                <a href="https://github.com/jamie-jones/Note-Taker-App" target="_blank" rel="noreferrer"><button class="btns"><h6>github repository</h6></button></a>
+                <div class="divider"/>
+                <a href="https://note-taker-jamie.herokuapp.com/" target="_blank" rel="noreferrer"><button class="btns"><h6>deployed application</h6></button></a>
+                </section>
             </div>
             </div>
         </section>
-        //         <figure class="img-section">
-        //         <a href="http://www.just-doc-it.com/" target="_blank" rel="noreferrer">
-        //         <img src={justDocIt} alt="Just Doc It"  class="portfolio-img"/>
-        //         </a>
-        //         <a href="https://jadavianett.github.io/Couchella/" target="_blank" rel="noreferrer">
-        //         <img src={Coachella} alt="Coachella"  class="portfolio-img"/>
-        //         </a>
-        //     </figure>
-        //         <br/>
-        //         <figure class="img-section">
-        //             <a href="http://www.positive-thoughts.org/" target="_blank" rel="noreferrer">
-        //                 <img src={PT} alt="Positive Thoughts" class="portfolio-img"/>
-        //             </a>
-        //         </figure>
-        // </div>
     );
 };
 
